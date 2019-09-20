0234b65e1fa625202206f1252f998c5f9b453429
Bernard <bsuwirjo@gmail.com>
2019-09-19 22:42:27 -0700

    Working LSTM, Need to test

--- lstm.py
+++ lstm.py
@@ -1,5 +1,98 @@
 import sys
+import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import keras
+
+TIME_STEPS = 3
+BATCH_SIZE = 6
+LEARNING_RATE = .0001
+OUTPUT_PATH = 'logs/'
 
 df_ge = pd.read_csv(sys.argv[1])
-df_ge.tail()
+ticker = sys.argv[1].split('/')[1].split('.')[0]
+
+plt.subplot(2, 1, 1)
+plt.plot(df_ge["Open"])
+plt.plot(df_ge["High"])
+plt.plot(df_ge["Low"])
+plt.plot(df_ge["Close"])
+plt.title('GE stock price history')
+plt.ylabel('Price (USD)')
+plt.xlabel('Days')
+plt.legend(['Open','High','Low','Close'], loc='upper left')
+
+plt.subplot(2,1,2)
+plt.plot(df_ge["Volume"])
+plt.title('GE stock volume history')
+plt.ylabel('Volume')
+plt.xlabel('Days')
+#plt.show()
+
+print("checking if any null values are present\n", df_ge.isna().sum())
+
+
+train_cols = ["Open","High","Low","Close","Volume"]
+df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
+print("Train and Test size", len(df_train), len(df_test))
+# scale the feature MinMax, build array
+x = df_train.loc[:,train_cols].values
+min_max_scaler = MinMaxScaler()
+x_train = min_max_scaler.fit_transform(x)
+x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+
+def build_timeseries(mat, y_col_index):
+    # y_col_index is the index of column that would act as output column
+    # total number of time-series samples would be len(mat) - TIME_STEPS
+    print(mat.shape)
+    dim_0 = mat.shape[0] - TIME_STEPS
+    dim_1 = mat.shape[1]
+    x = np.zeros((dim_0, TIME_STEPS, dim_1))
+    y = np.zeros((dim_0,))
+
+    #for i in tqdm_notebook(range(dim_0)):
+    for i in tqdm(range(dim_0)):
+        x[i] = mat[i:TIME_STEPS+i]
+        y[i] = mat[TIME_STEPS+i, y_col_index]
+    print("length of time-series i/o",x.shape,y.shape)
+    return x, y
+
+
+def trim_dataset(mat, batch_size):
+    """
+    trims dataset to a size that's divisible by BATCH_SIZE
+    """
+    no_of_rows_drop = mat.shape[0]%batch_size
+    if(no_of_rows_drop > 0):
+        return mat[:-no_of_rows_drop]
+    else:
+        return mat
+
+x_t, y_t = build_timeseries(x_train, 3)
+x_t = trim_dataset(x_t, BATCH_SIZE)
+y_t = trim_dataset(y_t, BATCH_SIZE)
+x_temp, y_temp = build_timeseries(x_test, 3)
+x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
+
+
+lstm_model = keras.models.Sequential()
+lstm_model.add(keras.layers.LSTM(100, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), dropout=0.0, recurrent_dropout=0.0, stateful=True,     kernel_initializer='random_uniform'))
+lstm_model.add(keras.layers.Dropout(0.5))
+lstm_model.add(keras.layers.Dense(20,activation='relu'))
+lstm_model.add(keras.layers.Dense(1,activation='sigmoid'))
+optimizer = keras.optimizers.RMSprop(learning_rate=LEARNING_RATE)
+lstm_model.compile(loss=keras.losses.mean_squared_error, optimizer=optimizer)
+
+csv_logger = keras.callbacks.callbacks.CSVLogger(os.path.join(OUTPUT_PATH,'LSTM-' + ticker + '.log'), append=True)
+
+history = lstm_model.fit(x_t, y_t, epochs=100, verbose=2, batch_size=BATCH_SIZE,
+                    shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                    trim_dataset(y_val, BATCH_SIZE)), callbacks=[csv_logger])
+
+
+
