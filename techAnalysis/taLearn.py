2ebd5a87f10991bf22e21fb57ab9d74948954e01
Bernard <bsuwirjo@gmail.com>
2020-01-27 23:36:51 -0800

    Created python program with commandline inputs

---
+++ techAnalysis/taLearn.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import ta
+import sys
+
+from PIL import Image
+
+from sklearn import preprocessing
+from sklearn.neural_network import MLPRegressor
+from sklearn.decomposition import PCA
+
+if len(sys.argv) != 4:
+    print("Error with command line arguments")
+else:
+    CSV = sys.argv[1]
+    PERIOD = int(sys.argv[2])
+    TARGET_PERIOD = int(sys.argv[3])
+
+def populateDataframe(csv):
+
+    df = pd.read_csv(csv)
+
+    # ## Momentum Indicators
+    df['Awesome'] = ta.momentum.ao(df['High'], df['Low'])
+    df['KAMA'] = ta.momentum.kama(df['Close'])
+    df['MFI'] = ta.momentum.money_flow_index(df['High'], df['Low'], df['Close'], df['Volume'])
+    df['ROC'] = ta.momentum.roc(df['Close'])
+    df['RSI'] = ta.momentum.rsi(df['Close'])
+    df['Stochastic'] = ta.momentum.stoch(df['High'], df['Low'], df['Close'])
+    df['Stochastic Signal'] = ta.momentum.stoch_signal(df['High'], df['Low'], df['Close'])
+    df['TSIIndicator'] = ta.momentum.tsi(df['Close'])
+    df['Ultimate'] = ta.momentum.uo(df['High'], df['Low'], df['Close'])
+    df['WilliamsRI'] = ta.momentum.wr(df['High'], df['Low'], df['Close'])
+    df['kama'] = ta.momentum.kama(df['Close'])
+    # ## Volume Indicators
+    df['ADI'] = ta.volume.acc_dist_index(df['High'], df['Low'], df['Close'], df['Volume'])
+    df['ChaikinMF'] = ta.volume.chaikin_money_flow(df['High'], df['Low'], df['Close'], df['Volume'])
+    df['EOM'] = ta.volume.ease_of_movement(df['High'], df['Low'], df['Volume'])
+    df['ForceIndex'] = ta.volume.force_index(df['Close'], df['Volume'])
+    df['NVI'] = ta.volume.negative_volume_index(df['Close'], df['Volume'])
+    df['OBV'] = ta.volume.on_balance_volume(df['Close'], df['Volume'])
+    df['EoM'] = ta.volume.sma_ease_of_movement(df['High'], df['Low'], df['Volume'])
+    df['VPT'] = ta.volume.volume_price_trend(df['Close'], df['Volume'])
+    df['ATR'] = ta.volatility.average_true_range(df['High'], df['Low'], df['Close'])
+    df['BolllingerHigh'] = ta.volatility.bollinger_hband(df['Close'])
+    df['BollingerHighIndicator'] = ta.volatility.bollinger_hband_indicator(df['Close'])
+    df['BollingerLow'] = ta.volatility.bollinger_lband(df['Close'])
+    df['BollingerLowIndicator'] = ta.volatility.bollinger_lband_indicator(df['Close'])
+    df['BollingerBandMovingAverage'] = ta.volatility.bollinger_mavg(df['Close'])
+    df['DonchianHigh'] = ta.volatility.donchian_channel_hband(df['Close'])
+    df['DonchianHighIndicator'] = ta.volatility.donchian_channel_hband_indicator(df['Close'])
+    df['DonchianLow'] = ta.volatility.donchian_channel_lband(df['Close'])
+    df['DonchianLowIndicator'] = ta.volatility.donchian_channel_lband_indicator(df['Close'])
+    df['KeltnerChannelCentral'] = ta.volatility.keltner_channel_central(df['High'], df['Low'], df['Close'])
+    df['KeltnerChannelHigh'] = ta.volatility.keltner_channel_hband(df['High'], df['Low'], df['Close'])
+    df['KeltnerChannelHighIndicator'] = ta.volatility.keltner_channel_hband_indicator(df['High'], df['Low'], df['Close'])
+    df['KeltnerChannelLow'] = ta.volatility.keltner_channel_lband(df['High'], df['Low'], df['Close'])
+    df['KeltnerChannelLowIndicator'] = ta.volatility.keltner_channel_lband_indicator(df['High'], df['Low'], df['Close'])
+    # ## Trend Indicators
+    df['ADX'] = ta.trend.adx(df['High'], df['Low'], df['Close'])
+    df['ADXNeg'] = ta.trend.adx_neg(df['High'], df['Low'], df['Close'])
+    df['ADXPos'] = ta.trend.adx_pos(df['High'], df['Low'], df['Close'])
+    df['AroonIndicator'] = ta.trend.aroon_down(df['Close'])
+    df['AroonUp'] = ta.trend.aroon_up(df['Close'])
+    df['CCI'] = ta.trend.cci(df['High'], df['Low'], df['Close'])
+    df['DPO'] = ta.trend.dpo(df['Close'])
+    df['EMA'] = ta.trend.ema_indicator(df['Close'])
+    df['Ichimoku_A'] = ta.trend.ichimoku_a(df['High'], df['Low'])
+    df['Ichimoku_B'] = ta.trend.ichimoku_b(df['High'], df['Low'])
+    df['KST'] = ta.trend.kst(df['Close'])
+    df['KSTSignal'] = ta.trend.kst_sig(df['Close'])
+    df['MACD'] = ta.trend.macd(df['Close'])
+    df['MACDDiff'] = ta.trend.macd_diff(df['Close'])
+    df['MACDSignal'] = ta.trend.macd_signal(df['Close'])
+    df['MassIndex'] = ta.trend.mass_index(df['High'], df['Low'])
+    df['PSAR_Down'] = ta.trend.psar_down(df['High'], df['Low'], df['Close'])
+    df['PSAR_DownIndicator'] = ta.trend.psar_down_indicator(df['High'], df['Low'], df['Close'])
+    df['PSAR_Up'] = ta.trend.psar_up(df['High'], df['Low'], df['Close'])
+    df['PSAR_UpIndicator'] = ta.trend.psar_up_indicator(df['High'], df['Low'], df['Close'])
+    df['TRIX'] = ta.trend.trix(df['Close'])
+    df['VortexIndicatorNeg'] = ta.trend.vortex_indicator_neg(df['High'], df['Low'], df['Close'])
+    df['VortexIndicatorPos'] = ta.trend.vortex_indicator_pos(df['High'], df['Low'], df['Close'])
+    df['CumulativeReturn'] = ta.others.cumulative_return(df['Close'])
+    df['DailyLogReturn'] = ta.others.daily_log_return(df['Close'])
+    df['DailyReturn'] = ta.others.daily_return(df['Close'])
+
+    return df;
+
+print("Populating Dataframe")
+df = populateDataframe(CSV)
+
+print("Calculating Labels from Target Period")
+for i in range(len(df)):
+    if i + TARGET_PERIOD >= len(df):
+        break
+    ## Compare to between close price and the targets close rpice
+    #difference = df.iloc[i + PERIOD].Close - df.iloc[i].Close
+
+    #For one day do difference in open and Close
+    difference = df.iloc[i + TARGET_PERIOD].Close - df.iloc[i+TARGET_PERIOD].Open
+
+    #Can do binary here or regression
+    #if(difference > 0):
+    df.loc[i, 'Label'] = difference
+
+    #print(difference)
+
+# In[67]:
+df.to_csv("TmpDataframe.csv", sep='\t')
+
+df = df.drop(['Date'], axis=1)
+df = df.fillna(0)
+df.head()
+
+
+# ## Clean Data
+
+labels = df["Label"]
+
+#Normalize the Data
+dforiginal = df
+x = df.values
+scaler = preprocessing.MinMaxScaler()
+xScaled = scaler.fit_transform(x)
+df = pd.DataFrame(xScaled)
+df.columns = dforiginal.columns
+df.head()
+
+
+"""
+# Dimensionality Reduction
+PCA_COMPONENTS = 8
+
+pca = PCA(n_components=PCA_COMPONENTS)
+pca.fit(df)
+df = pd.DataFrame(pca.transform(df), columns=['PCA%i' % i for i in range(PCA_COMPONENTS)], index=df.index)
+"""
+
+df["Labels"] = labels
+
+dataArray = df.to_numpy()
+
+
+print("Entering Program")
+start = 1000;
+end = start + PERIOD;
+
+
+# In[72]:
+
+profit = 0
+wins = 0
+losses = 0
+
+while end + 1 < len(df):
+
+    targetAtts = dataArray[end,:-1]
+    targetAtts = targetAtts.reshape(1, -1)
+
+    targetLabel = dataArray[end,-1]
+    xTrain = dataArray[start:end - 1,:-1]
+    yTrain = dataArray[start:end -1,-1]
+
+    #print(targetAtts.shape)
+    #print(xTrain.shape)
+
+    model = MLPRegressor(solver = 'adam', activation = 'relu', hidden_layer_sizes = [128,128, 128, 128,128, 128, 128], momentum = 1)
+    model.fit(xTrain, yTrain)
+    pred = model.predict(targetAtts)[0]
+
+    #Do something with percentage here
+
+    print(pred, " | ", targetLabel)
+
+    if pred > 0:
+        print("--------------")
+
+
+        """
+        targetOpen = dataArray[end + 1][0]
+        targetClose = dataArray[end + 1][3]
+        dayProfit = targetClose - targetOpen
+        """
+
+        profit += targetLabel
+        print("Current Profit:" , profit)
+        if targetLabel > 0:
+            wins += 1
+        else:
+            losses += 1
+        print("Wins: ", wins, " | Losses:", losses)
+
+    start += 1
+    end += 1
+print("Total Profit:", profit)
+print("Win Percentage:", wins/losses + wins)
