34707d44ed4674812f5171fba15a8bde9a7cd641
Bernard <bsuwirjo@gmail.com>
2020-02-03 23:08:17 -0800

    Added testRobinhood.py trying to get it to work

---
+++ testRobinhood.py
@@ -0,0 +1,29 @@
+from Robinhood import Robinhood # Robinhood api to check/buy/sell stocks
+#from bs4 import BeautifulSoup # Webscraper
+#import requests
+import datetime # Getting current time
+import sys
+#import math # math.floor, making sure we don't overbuy stocks
+from pytz import timezone # Change timezone to est
+tz = timezone('EST')
+
+
+fileName = open("../cred", "r")
+contents = fileName.read()
+user = contents.split()[0]
+pwd = contents.split()[1]
+fileName.close
+
+print(user)
+print(pwd)
+
+#Log into Robinhood
+trader = Robinhood()
+login = trader.login(username=user, password=pwd)
+if login == True:
+    string = "Login Successful\n"
+    printStr(string, printOutput, f)
+else:
+    string = "Login Failed, Exiting"
+    printStr(string, printOutput, f)
+    sys.exit()
