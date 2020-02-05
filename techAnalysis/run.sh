bed6bbf1d9868d4d61b287928396873dab49ccd4
Bernard <bsuwirjo@gmail.com>
2020-02-05 12:54:38 -0800

    updated run.sh

--- techAnalysis/run.sh
+++ techAnalysis/run.sh
@@ -1,33 +1,33 @@
-python3.6 -W ignore -u taLearn.py data/AMZN.csv 100 1 2018-05-05 &> output/AMZN100.txt
-python3.6 -W ignore -u taLearn.py data/goog.csv 100 1 2018-05-05 &> output/GOOG100.txt
-python3.6 -W ignore -u taLearn.py data/NFLX.csv 100 1 2018-05-05 &> output/NFLX100.txt
-python3.6 -W ignore -u taLearn.py data/STE.csv 100 1 2018-05-05 &> output/STE100.txt
-python3.6 -W ignore -u taLearn.py data/TSLA.csv 100 1 2018-05-05 &> output/TSLA100.txt
+python3.6 -W ignore -u taLearn.py AMZN 100 1 2018-05-05 &> output/AMZN100.txt
+python3.6 -W ignore -u taLearn.py GOOG 100 1 2018-05-05 &> output/GOOG100.txt
+python3.6 -W ignore -u taLearn.py NFLX 100 1 2018-05-05 &> output/NFLX100.txt
+python3.6 -W ignore -u taLearn.py STE 100 1 2018-05-05 &> output/STE100.txt
+python3.6 -W ignore -u taLearn.py TSLA 100 1 2018-05-05 &> output/TSLA100.txt
 
-python3.6 -W ignore -u taLearn.py data/dad/ADSK.csv 100 1 2018-05-05 &> output/dad/ADSK100.txt
-python3.6 -W ignore -u taLearn.py data/dad/CMG.csv 100 1 2018-05-05 &> output/dad/CMG100.txt
-python3.6 -W ignore -u taLearn.py data/dad/INTU.csv 100 1 2018-05-05 &> output/dad/INTU100.txt
-python3.6 -W ignore -u taLearn.py data/dad/RRGB.csv 100 1 2018-05-05 &> output/dad/RRGB100.txt
+python3.6 -W ignore -u taLearn.py ADSK 100 1 2018-05-05 &> output/ADSK100.txt
+python3.6 -W ignore -u taLearn.py CMG 100 1 2018-05-05 &> output/CMG100.txt
+python3.6 -W ignore -u taLearn.py INTU 100 1 2018-05-05 &> output/INTU100.txt
+python3.6 -W ignore -u taLearn.py RRGB 100 1 2018-05-05 &> output/RRGB100.txt
 
-python3.6 -W ignore -u taLearn.py data/AMZN.csv 250 1 2018-05-05 &> output/AMZN250.txt
-python3.6 -W ignore -u taLearn.py data/goog.csv 250 1 2018-05-05 &> output/GOOG250.txt
-python3.6 -W ignore -u taLearn.py data/NFLX.csv 250 1 2018-05-05 &> output/NFLX250.txt
-python3.6 -W ignore -u taLearn.py data/STE.csv 250 1 2018-05-05 &> output/STE250.txt
-python3.6 -W ignore -u taLearn.py data/TSLA.csv 250 1 2018-05-05 &> output/TSLA250.txt
+python3.6 -W ignore -u taLearn.py AMZN 250 1 2018-05-05 &> output/AMZN250.txt
+python3.6 -W ignore -u taLearn.py GOOG 250 1 2018-05-05 &> output/GOOG250.txt
+python3.6 -W ignore -u taLearn.py NFLX 250 1 2018-05-05 &> output/NFLX250.txt
+python3.6 -W ignore -u taLearn.py STE 250 1 2018-05-05 &> output/STE250.txt
+python3.6 -W ignore -u taLearn.py TSLA 250 1 2018-05-05 &> output/TSLA250.txt
 
-python3.6 -W ignore -u taLearn.py data/dad/ADSK.csv 250 1 2018-05-05 &> output/dad/ADSK250.txt
-python3.6 -W ignore -u taLearn.py data/dad/CMG.csv 250 1 2018-05-05 &> output/dad/CMG250.txt
-python3.6 -W ignore -u taLearn.py data/dad/INTU.csv 250 1 2018-05-05 &> output/dad/INTU250.txt
-python3.6 -W ignore -u taLearn.py data/dad/RRGB.csv 250 1 2018-05-05 &> output/dad/RRGB250.txt
+python3.6 -W ignore -u taLearn.py ADSK 250 1 2018-05-05 &> output/ADSK250.txt
+python3.6 -W ignore -u taLearn.py CMG 250 1 2018-05-05 &> output/CMG250.txt
+python3.6 -W ignore -u taLearn.py INTU 250 1 2018-05-05 &> output/INTU250.txt
+python3.6 -W ignore -u taLearn.py RRGB 250 1 2018-05-05 &> output/RRGB250.txt
 
-python3.6 -W ignore -u taLearn.py data/AMZN.csv 500 1 2018-05-05 &> output/AMZN50.txt
-python3.6 -W ignore -u taLearn.py data/goog.csv 500 1 2018-05-05 &> output/GOOG50.txt
-python3.6 -W ignore -u taLearn.py data/NFLX.csv 500 1 2018-05-05 &> output/NFLX50.txt
-python3.6 -W ignore -u taLearn.py data/STE.csv 500 1 2018-05-05 &> output/STE50.txt
-python3.6 -W ignore -u taLearn.py data/TSLA.csv 500 1 2018-05-05 &> output/TSLA50.txt
+python3.6 -W ignore -u taLearn.py AMZN 500 1 2018-05-05 &> output/AMZN50.txt
+python3.6 -W ignore -u taLearn.py GOOG 500 1 2018-05-05 &> output/GOOG50.txt
+python3.6 -W ignore -u taLearn.py NFLX 500 1 2018-05-05 &> output/NFLX50.txt
+python3.6 -W ignore -u taLearn.py STE 500 1 2018-05-05 &> output/STE50.txt
+python3.6 -W ignore -u taLearn.py TSLA 500 1 2018-05-05 &> output/TSLA50.txt
 
-python3.6 -W ignore -u taLearn.py data/dad/ADSK.csv 500 1 2018-05-05 &> output/dad/ADSK50.txt
-python3.6 -W ignore -u taLearn.py data/dad/CMG.csv 500 1 2018-05-05 &> output/dad/CMG50.txt
-python3.6 -W ignore -u taLearn.py data/dad/INTU.csv 500 1 2018-05-05 &> output/dad/INTU50.txt
-python3.6 -W ignore -u taLearn.py data/dad/RRGB.csv 500 1 2018-05-05 &> output/dad/RRGB50.txt
+python3.6 -W ignore -u taLearn.py ADSK 500 1 2018-05-05 &> output/ADSK50.txt
+python3.6 -W ignore -u taLearn.py CMG 500 1 2018-05-05 &> output/CMG50.txt
+python3.6 -W ignore -u taLearn.py INTU 500 1 2018-05-05 &> output/INTU50.txt
+python3.6 -W ignore -u taLearn.py RRGB 500 1 2018-05-05 &> output/RRGB50.txt
 
