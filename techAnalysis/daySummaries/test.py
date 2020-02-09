9e1bcbd3e1cf9392241a42f15f628196307c348b
Bernard <bsuwirjo@gmail.com>
2020-02-09 15:38:54 -0800

    Added daySummaries for Ameritrade stock csvs

---
+++ techAnalysis/daySummaries/test.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import sys
+
+if len(sys.argv) != 2:
+    print("Error with command line arguments")
+    sys.exit()
+else:
+    DATE = sys.argv[1] #MONTH/DAY/YEAR
+
+data = pd.read_csv("transactions.csv")
+
+stockDict = {};
+
+"""
+stockTemplate = {
+      "Stock": ""
+      "BuyPrice": 0,
+      "SellPrice": 0,
+      "Quantity": 0,
+      "Profit": 0,
+      "ProfitPercentage": 0,
+}
+"""
+
+#print(data.columns)
+
+#GET COLUMN
+#print(data["SYMBOL"])
+
+#Get Row
+#print(data.iloc[2])
+
+
+#Populate Stock Dictionary
+for index, row in data.iterrows():
+    if row["DATE"] != DATE:
+        continue
+    #print(row)
+
+    if row["DESCRIPTION"].split()[0] == "Bought":
+        stockDict[row["SYMBOL"]] =  {
+              "BuyPrice": 0,
+              "SellPrice": 0,
+              "Quantity": 0,
+              "Profit": 0,
+              "ProfitPercentage": 0,
+        }
+        stockObj = stockDict[row["SYMBOL"]]
+        stockObj["BuyPrice"] = row["PRICE"]
+        stockObj["Quantity"] = row["QUANTITY"]
+
+    elif row["DESCRIPTION"].split()[0] == "Sold":
+        stockObj = stockDict[row["SYMBOL"]]
+        stockObj["SellPrice"] = row["PRICE"]
+
+    else:
+        print("There was an error fix it")
+
+print("Stock Specifics:")
+for key, value in stockDict.items():
+    bought = value["BuyPrice"]
+    sold = value["SellPrice"]
+    profit = (sold - bought) * value["Quantity"]
+    profitPercentage = (sold - bought)/bought
+    stockDict[key]["Profit"] = profit
+    stockDict[key]["ProfitPercentage"] = profitPercentage
+    print(key, "| Profit: ", profit, "| Profit Percentage: ", profitPercentage, "\n")
+
+
+profitActual = 0
+profitPercentage = 0
+for key, value in stockDict.items():
+    profitActual += value["Profit"]
+    profitPercentage += value["ProfitPercentage"]
+
+
+print("Day Summary")
+print("Total Profit: ", profitActual)
+print("Total Profit Percentage: ", profitPercentage)
--- techAnalysis/test/test.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("transactions.csv")
-print(data.head())
